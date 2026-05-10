fdb2128138d6c61d1bf4ccb629f3dd8abda60250
maksim.samorukov <maxim.samorukov@gmail.com>
2026-05-10 21:34:08 +0300

    seo

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [
+    react(),
+    svgr(),
+    Sitemap({ hostname: "https://tg-bots-site-6uey.vercel.app" }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
